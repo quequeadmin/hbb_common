9820d29c1c825d92e0da7f6387026a21b5845006
queque1 <553769675@qq.com>
2025-04-15 17:58:02 +0800

    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -98,7 +98,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["www.schuangtu.com"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.schuangtu.cn"];
 pub const RS_PUB_KEY: &str = "queque123";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
